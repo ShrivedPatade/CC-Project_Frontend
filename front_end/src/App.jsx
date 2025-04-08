17ff376ae4f5f5273ebb963fc69250320b0f3c2a
ShrivedPatade <114217113+ShrivedPatade@users.noreply.github.com>
2025-04-08 14:25:53 +0530

    Update App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
     setLoading(true);
 
     try {
-      const postResponse = await fetch('https://74.225.250.3:5000/chatbot', {
+      const postResponse = await fetch('/api/chatbot', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query, k: 3 }),
